b4b569a56a4803dd2143a726c45c62fc46e080ad
mooreotavio <otaviomoore@gmail.com>
2023-08-25 04:57:59 -0300

    ainda mexendo nas env vars

--- Database.php
+++ Database.php
@@ -6,7 +6,7 @@ class Database
     public function __construct(){
         
 
-        $this->connection = new PDO('mysql:host=' . $_ENV['MYSQLHOST'] . ';dbname='. $_ENV['MYSQLDATABASE'] .';charset=utf8mb4', $_ENV['MYSQLUSER'], $_ENV['MYSQLPASSWORD']);
+        $this->connection = new PDO("mysql:host=" . $_ENV['MYSQLHOST'] . ";dbname=". $_ENV['MYSQLDATABASE'] .";charset=utf8mb4", $_ENV['MYSQLUSER'], $_ENV['MYSQLPASSWORD']);
     }
     public function query($query)
     {
