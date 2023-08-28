a56128335784976650ad7e17c4dc9d00acbaf91c
mooreotavio <otaviomoore@gmail.com>
2023-08-28 02:22:52 -0300

    setting html title

--- controllers/index.php
+++ controllers/index.php
@@ -10,7 +10,7 @@ require('./Database.php')
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Clinica</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
 </head>
 
--- controllers/pacientes.php
+++ controllers/pacientes.php
@@ -12,7 +12,7 @@ $pacients = $db->query("select * from pacientes");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pacientes</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
 </head>
 
